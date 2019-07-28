a1cad899cd50cdfd280ff6c686537fe63f06d63c
alexrdz <drinkhorchata@gmail.com>
2019-07-28 09:09:47 -0500

    make sidenav reusable with props.children

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min";
-import { Router, RouteComponentProps } from "@reach/router";
+import { Router, RouteComponentProps, Link } from "@reach/router";
 import { NavBar } from "./UI/nav/NavBar";
 import { SideNav } from "./UI/nav/SideNav";
 import { Home } from "./Routes/home/Home";
 import { Contact } from "./Routes/contact/Contact";
+import { NavItem } from "reactstrap";
 
 type Props = { component: React.ComponentType } & RouteComponentProps;
 const Route: React.FunctionComponent<Props> = ({
@@ -22,13 +23,34 @@ class App extends React.Component<{}, { toggle: boolean }> {
         <NavBar />
         <div className="container-fluid">
           <div className="row">
-            <SideNav />
-						<div className="col-12 col-md-9">
-							<Router>
-								<Route path="/" component={Home} />
-								<Route path="/contact" component={Contact} />
-							</Router>
-						</div>
+            <SideNav>
+              <NavItem>
+                <Link className="nav-link" to="#">
+                  Link
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="#">
+                  Link
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="#">
+                  Another Link
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="#">
+                  Last Link
+                </Link>
+              </NavItem>
+            </SideNav>
+            <div className="col-12 col-md-9">
+              <Router>
+                <Route path="/" component={Home} />
+                <Route path="/contact" component={Contact} />
+              </Router>
+            </div>
           </div>
         </div>
       </React.Fragment>
